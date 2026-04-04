a899e8b6a6e9738b0b76750aefffaff9e361e951
jinchung <jin.chung17@gmail.com>
2026-04-03 22:31:08 -0400

    MemoryArray

--- src/MemoryArray.sol
+++ src/MemoryArray.sol
@@ -3,8 +3,12 @@ pragma solidity 0.8.28;
 
 contract MemoryArray {
     function main(uint8 a) public returns (uint256[] memory) {
-        // return an array that goes from 0 to a
-        // for example, if a is 3, return [0, 1, 2]
-
+      // return an array that goes from 0 to a
+      // for example, if a is 3, return [0, 1, 2]
+      uint256[] memory result = new uint256[](a);
+      for (uint256 i = 0; i < a; i++) {
+        result[i] = i;
+      }
+      return result;
     }
 }
