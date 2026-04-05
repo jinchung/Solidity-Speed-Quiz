4eb87c607eb3dcfed840d1efc7d45cb7b013bc08
jinchung <jin.chung17@gmail.com>
2026-04-05 10:31:06 -0400

    TryCatchReason

--- src/TryCatchReason.sol
+++ src/TryCatchReason.sol
@@ -7,11 +7,15 @@ interface IRare {
 
 contract TryCatchReason {
     function main(address a, uint256 x) public returns (bytes memory) {
-        // use try catch to call "a.rare(x)" where x is a uint256
-        // rare(uint256) does not return anything
-        // if the call doesn't revert, return an empty string
-        // if the call reverts, return the reason of the revert
-        // you will need to create the interface yourself
-
+      // use try catch to call "a.rare(x)" where x is a uint256
+      // rare(uint256) does not return anything
+      // if the call doesn't revert, return an empty string
+      // if the call reverts, return the reason of the revert
+      // you will need to create the interface yourself
+      try IRare(a).rare(x) {
+        return "";
+      } catch (bytes memory data) {
+        return data;
+      }
    }
 }
