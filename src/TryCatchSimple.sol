3ca3788f9ce3f0e4676914185604b4751e70deb2
jinchung <jin.chung17@gmail.com>
2026-04-05 10:19:32 -0400

    TryCatchSimple

--- src/TryCatchSimple.sol
+++ src/TryCatchSimple.sol
@@ -6,7 +6,12 @@ interface IERC20 {
 
 contract TryCatchSimple {
     function main(IERC20 token) public view returns (string memory) {
-        // use try catch to return the name of the token using .name()
-        // if the call fails, return an empty string
+      // use try catch to return the name of the token using .name()
+      // if the call fails, return an empty string
+      try token.name() returns (string memory result) {
+        return result;
+      } catch {
+        return "";
+      }
     }
 }
