5102f729ab1a9fff3d9d78673abbf2bb443c1432
jinchung <jin.chung17@gmail.com>
2026-04-06 15:22:36 -0400

    ConvertToNegative

--- src/ConvertToNegative.sol
+++ src/ConvertToNegative.sol
@@ -5,8 +5,19 @@ contract ConvertToNegative {
     error TooBig();
 
     function convertToNegative(uint256 x) external pure returns (int256) {
-        // return -x
-        // if -x cannot be computed, revert with "TooBig()"
-        // do not hardcode any constants
+      // return -x
+      // if -x cannot be computed, revert with "TooBig()"
+      // do not hardcode any constants
+      uint256 beforeConversionLimit = uint256(type(int256).max) + 1;
+
+      if (x > beforeConversionLimit) {
+        revert TooBig();
+      }
+
+      if (x == beforeConversionLimit) {
+        return type(int256).min;
+      }
+
+      return -int256(x);
     }
 }
