af18c8a994dd1809e571acda9037aa7a7da636b9
jinchung <jin.chung17@gmail.com>
2026-04-06 11:47:46 -0400

    IsContract

--- src/IsContract.sol
+++ src/IsContract.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.28;
 contract IsContract {
 
     function isContract(address addr) public view returns (bool) {
-        // return true if the address has bytecode
-        // sidenote: as of EIP-7702, EOAs can have bytecode
-
+      // return true if the address has bytecode
+      // sidenote: as of EIP-7702, EOAs can have bytecode
+      return (addr.code.length > 0);
     }
 }
